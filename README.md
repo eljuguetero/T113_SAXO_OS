abd2214c496371bb90ba2e2bdaac6fa4b093e1ac
Andres CALDERON <andresfcalderon@gmail.com>
2026-02-12 20:28:01 -0500

    u-boot fixes

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 sudo apt update
 sudo apt install -y build-essential fakeroot bc bison flex libssl-dev libncurses-dev libelf-dev  gcc-arm-linux-gnueabi
-sudo apt install -y mkbootimg sudo apt install u-boot-tools
-
+sudo apt install -y mkbootimg sudo apt install u-boot-tools python3-dev
 git clone --recurse-submodules --shallow-submodules --depth 1 git@github.com:eljuguetero/T113_SAXO_OS.git
--- build_u-boot.sh
+++ build_u-boot.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 cd $SCRIPT_DIR
 
-cp u-boot-patch-v2025.07/t113s_saxo_gateway_defconfig u-boot/configs
+cp u-boot-patch-v2025.07/t113s_saxo_defconfig u-boot/configs
 cp u-boot-patch-v2025.07/sun8i-t113s-saxo.dts         u-boot/arch/arm/dts
 cp u-boot-patch-v2025.07/sunxi-d1s-t113s-saxo.dtsi    u-boot/arch/arm/dts
 cp u-boot-patch-v2025.07/sunxi-d1s-t113.dtsi          u-boot/arch/riscv/dts
@@ -13,7 +13,7 @@ cd u-boot
 
 git checkout -f
 
-patch -d . -p1 <  ../u-boot-patch-v2025.07/0001-t113s_saxo-dtb.patch
+patch -d . -p1 <  ../u-boot-patch-v2025.07/0001-saxo-dtb.patch
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j4 t113s_saxo_gateway_defconfig
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j4 t113s_saxo_defconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j4
